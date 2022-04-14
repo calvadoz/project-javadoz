d5159f75573840ef033f8ea59ac6c80cbc0aba2c
Kelvin Wu <wu.kelvin91@gmail.com>
2022-04-14 14:52:41 +0800

    updated cors

--- server.js
+++ server.js
@@ -14,23 +14,16 @@ const {
   scrapeJavDbActress,
 } = require("./scraper");
 
-var allowedOrigins = ["http://localhost:3000", "https://calvadoz.github.io"];
-// app.use(
-//   cors({
-//     origin: function (origin, callback) {
-//       // allow requests with no origin
-//       // (like mobile apps or curl requests)
-//       if (!origin) return callback(null, true);
-//       if (allowedOrigins.indexOf(origin) === -1) {
-//         var msg =
-//           "The CORS policy for this site does not " +
-//           "allow access from the specified Origin.";
-//         return callback(new Error(msg), false);
-//       }
-//       return callback(null, true);
-//     },
-//   })
-// );
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
+  );
+  next();
+});
 app.use(
   cors({
     origin: "*",
