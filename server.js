5d624354e7818b7aee70bf6084da6b3c8e2e500f
Kelvin Wu <wu.kelvin91@gmail.com>
2022-04-14 12:46:46 +0800

    update cors

--- server.js
+++ server.js
@@ -15,23 +15,23 @@ const {
 } = require("./scraper");
 
 var allowedOrigins = ["http://localhost:3000", "https://calvadoz.github.io"];
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      // allow requests with no origin
-      // (like mobile apps or curl requests)
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        var msg =
-          "The CORS policy for this site does not " +
-          "allow access from the specified Origin.";
-        return callback(new Error(msg), false);
-      }
-      return callback(null, true);
-    },
-  })
-);
-// app.use(cors());
+// app.use(
+//   cors({
+//     origin: function (origin, callback) {
+//       // allow requests with no origin
+//       // (like mobile apps or curl requests)
+//       if (!origin) return callback(null, true);
+//       if (allowedOrigins.indexOf(origin) === -1) {
+//         var msg =
+//           "The CORS policy for this site does not " +
+//           "allow access from the specified Origin.";
+//         return callback(new Error(msg), false);
+//       }
+//       return callback(null, true);
+//     },
+//   })
+// );
+app.use(cors());
 app.use(express.json());
 app.use(helmet());
 app.use("/static", express.static("assets", { maxAge: 3600000 }));
